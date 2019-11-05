46708cd0b121937871a94f20df77557b8bb1cbee
Miguel Solans <dev.miguelsolans@gmail.com>
2019-11-05 10:45:48 +0000

    Update server.js

--- config/server.js
+++ config/server.js
@@ -23,27 +23,26 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 // Define Routes
-const FilmesRoutes = require('../app/routes/index');
+const RootRoutes = require('../app/routes/index');
 
 // Webapp Filmes Routes
-app.use('/', FilmesRoutes);
+app.use('/', RootRoutes);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     next(createError(404));
-  });
-  
-  // error handler
-  app.use(function(err, req, res, next) {
+});
+// error handler
+app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+
     // render the error page
     res.status(err.status || 500);
     res.render('error');
-  });
-  
+});
 
 // Module Export
 module.exports = app;
